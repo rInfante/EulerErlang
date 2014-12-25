4cf701c6829ff69b4820bb70436a7570eb663d13
rInfante <robertoinfante@devqf.com>
2014-12-25 21:09:56 +0000

    Added new solution to problem 3
    This goes out of memory because it uses eager lists. I am going to
    create a new solution (probl;me_3_c) working with lazy lists.

---
+++ problem_00003_b.erl
@@ -0,0 +1,31 @@
+-module(problem_00003_b).
+
+-export([prime_divisors/1]).
+
+%%DEBUG
+-export([divisors/1, is_prime/1]).
+
+divisors(N) ->
+	Previous = lists:seq(2, N-1),
+	lists:filter(fun(P)-> N rem P == 0 end, Previous).
+	
+is_prime(N) ->
+	if 
+		N =<3 ->
+			N >= 2;
+		(N rem 2 == 0) or (N rem 3 == 0) ->
+			false;
+		true ->
+			PotentialDivisors = lists:seq(5,trunc(math:sqrt(N)),6),
+			case lists:any(fun(P)->(N rem P == 0) or (N rem (P+2) == 0) end, PotentialDivisors) of
+				true -> false;
+				false -> true			
+			end
+	end.
+	
+prime_divisors(N) ->
+	Divisors = divisors(N),
+	lists:filter(fun(D) -> is_prime(D) end, Divisors).
+	
+
+	
