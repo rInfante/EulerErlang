715be9b54cc612a825864d64ddde4a27fbd5acfd
rInfante <robertoinfante@devqf.com>
2014-12-26 17:14:44 +0000

    Problem 3 with lazy lists. Not correct yet.
    Still to fix lazy list functions so that they all return a function
    rather than executing eagerly

---
+++ problem_00003_c.erl
@@ -0,0 +1,75 @@
+-module(problem_00003_c).
+
+-export([prime_divisors/1]).
+
+%%DEBUG
+-export([divisors/1, is_prime/1, from/1, filter/2, take/2, from_to/2, from_to_step/3, for_each/2]).
+
+divisors(N) ->
+	filter(fun(P)-> N rem P == 0 end, from_to(2, N-1)).
+	
+is_prime(N) ->
+	if 
+		N =<3 ->
+			N >= 2;
+		(N rem 2 == 0) or (N rem 3 == 0) ->
+			false;
+		true ->
+			case any(fun(P)->(N rem P == 0) or (N rem (P+2) == 0) end, from_to_step(5,trunc(math:sqrt(N)),6)) of
+				true -> false;
+				false -> true			
+			end
+	end.
+	
+prime_divisors(N) ->
+	for_each(fun(PD)->io:format("Prime Divisor: ~w~n",[PD]) end, filter(fun(D) -> is_prime(D) end, divisors(N))).
+	
+%lazy sequence starting from K: eager head, lazy tail
+from(K)	->
+	[K|fun()->from(K+1) end].
+
+%filter for lazy sequences
+filter(_, []) -> [];
+filter(Pred,[Hd|Tl]) ->
+	case Pred(Hd) of
+		true -> 
+			[Hd | fun() -> filter(Pred, Tl()) end];
+		false -> 
+			filter(Pred,Tl())
+	end.
+	
+any(_, []) -> false;
+any(Pred,[Hd|Tl]) ->
+	case Pred(Hd) of
+		true -> 
+			true;
+		false -> 
+			any(Pred,Tl())
+	end.	
+	
+%take function for lazy sequences
+%You pass in the second parameter a function acting on lazy sequences	
+take({0,_}, _) 
+	-> [];
+take({N,_}, _) when N < 0 
+	-> [];	
+take({N,S}, [Hd|Tl])->
+	%DEBUG:io:format("processing ~w; N:~w; S:~w~n", [Hd,N,S]),
+	Tl1 = Tl(),
+	[Hd| fun() -> take({N-S,S}, Tl1) end].
+	
+from_to_step(N, M, S) ->
+	NumItems = M - N + 1,
+	take({NumItems, S}, from(N)).
+	
+from_to(N, M) ->
+	from_to_step(N, M, 1).
+
+for_each(_, []) -> [];
+for_each(Action, [Hd|Tl]) ->
+	Action(Hd),
+	Tl1 = Tl(),
+	for_each(Action, Tl1).
+	
+	
+	
